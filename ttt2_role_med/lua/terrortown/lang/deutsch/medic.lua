ef0866a56d318a490a10623ee72bc790ec897c95
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-09-10 12:36:18 +0200

    updated german translation

--- ttt2_role_med/lua/terrortown/lang/deutsch/medic.lua
+++ ttt2_role_med/lua/terrortown/lang/deutsch/medic.lua
@@ -6,15 +6,15 @@ Bleib am leben und hilf allen, solange es möglich ist!]]
 L[ "body_found_" .. MEDIC.abbr ] = "Er war ein Sanitäter..."
 L[ "search_role_" .. MEDIC.abbr ] = "Diese Person war ein Sanitäter!"
 L[ "target_" .. MEDIC.name ] = "Sanitäter"
-L[ "ttt2_desc_" .. MEDIC.name ] = [[Der Sanitäter muss jeden mit all seiner Macht mit Medizin versorgen (WIRKLICH JEDEN), dafür hast du die Medigun und einen Defribillator.
+L[ "ttt2_desc_" .. MEDIC.name ] = [[Der Sanitäter muss jeden mit all seiner Macht heilen (WIRKLICH JEDEN), dafür hat er die Medigun und einen Defribillator.
 Versuche, allen zu helfen!]]
-L[ "ttt2_role_medic_popuptitle" ] = "Der Sanitäter, der auf dem Feld erschienen ist, heißt: "
-L[ "ttt2_role_medic_popuptitle_2" ] = "Der Saniäter wurde getötet! Ein Kriegsverbrechen fand gerade statt von: "
-L[ "ttt2_role_medic_popuptitle_3" ] = "Der Sanitäter hat ein Verbrechen begangen! Er ist jetzt ein Medic ohne Ausrüstung der den Unschuldigen hilft: "
+L[ "ttt2_role_medic_popuptitle" ] = "Ein Sanitäter ist auf dem Feld angekommen. Der Name lautet: "
+L[ "ttt2_role_medic_popuptitle_2" ] = "Der Saniäter wurde getötet! Dieses Kriegsverbrechen wurde begangen von: "
+L[ "ttt2_role_medic_popuptitle_3" ] = "Der Sanitäter hat ein Verbrechen begangen! Er hat seine Ausrüstung verloren und hilft nun den Unschuldigen: "
 L[ "ttt2_role_medic_popuptitle_4" ] = "Der Sanitäter hat einen anderen Sanitäter getötet! Das ist Verrat, tötet: "
 L[ "weapon_med_defi_name" ] = "Sanitäter Defibrillator"
 L[ "weapon_med_defi_desc" ] = "Ein hochenergetisches Gerät zum Wiederbeleben anderer Spieler."
-L[ "med_revived_by_player" ] = "Du wirst von {name} wiederbelebt. Halte dich bereit!"
+L[ "med_revived_by_player" ] = "Sie werden durch {name} wiederbelebt. Bereiten Sie sich vor!"
 L[ "med_defi_hold_key_to_revive" ] = "Halte [{key}] um Spieler wiederzubeleben"
 L[ "med_defi_revive_progress" ] = "Zeit übrig: {time}s"
 L[ "med_defi_charging" ] = "Defibrillator lädt sich auf, bitte warten"
@@ -29,4 +29,4 @@ L[ "med_defi_error_failed" ] = "Wiederbeleben fehlgeschlagen. Bitte versuche es 
 L[ "ttt2_med_defibrillator_help1" ] = "{primaryfire} Leichen wiederbeleben."
 L[ "ttt2_med_defibrillator_help2" ] = "{secondaryfire} Piepton abspielen."
 L[ "ttt2_med_medigun_help1" ] = "{primaryfire} Heile jemanden mit einem starken Heilstrahl."
-L[ "ttt2_med_medigun_help2" ] = "{secondaryfire} Verwenden Sie Ubercharge, sobald es aufgeladen ist, um Ihren Strahl stärker zu machen."
+L[ "ttt2_med_medigun_help2" ] = "{secondaryfire} Verwenden Sie nach dem Aufladen Ubercharge, um Ihren Strahl stärker zu machen."
