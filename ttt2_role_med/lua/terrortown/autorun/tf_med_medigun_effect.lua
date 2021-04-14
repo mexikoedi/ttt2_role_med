a5f6ac36ff54dca804555fc1adc0ef68b0f5ebdd
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-04-14 23:20:44 +0200

    moved + removed unnecessary code

--- ttt2_role_med/lua/terrortown/autorun/tf_med_medigun_effect.lua
+++ ttt2_role_med/lua/terrortown/autorun/tf_med_medigun_effect.lua
@@ -1,4 +1,3 @@
-if engine.ActiveGamemode() ~= "terrortown" then return end -- we need this (or we can have this) to prevent this file from loading if the gamemmode is not ttt, this is only needed here lua/autorun, in /terrortown/... is this not needed
 game.AddParticles("particles/ttt_medigun/medicgun_beam.pcf") -- added/loaded the particles for the medigun that are called in weapon_ttt2_medic_medigun
 game.AddParticles("particles/ttt_medigun/ownmedicgun_beam.pcf")
 game.AddParticles("particles/ttt_medigun/ownmedicgun_attrib.pcf")
